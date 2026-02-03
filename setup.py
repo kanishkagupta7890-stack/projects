b69812873675a339bd9435078c62f71a31688a1e
kanishkagupta7890-stack <kanishkagupta7890@gmail.com>
2026-02-03 19:29:59 +0530

    EDA and problems statement

--- setup.py
+++ setup.py
@@ -21,7 +21,18 @@ setup(
     version="0.1",
     author="kanishkagupta",
     packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+    install_requires=get_requirements('requirements.txt'),
+    python_requires='>=3.8.0',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ]
       
 )
 
